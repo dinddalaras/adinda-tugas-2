af14d631c1db7ea6f7c2a3be3608be9a92de9422
dinddalaras <dinddalarasaty123@gmail.com>
2022-03-30 20:42:53 +0700

    Tugas Pertemuan 2

---
+++ application/controllers/biodata.php
@@ -0,0 +1,10 @@
+<?php
+class biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Adinda Nurul Larasaty
+        saya tinggal di daerah Kemayoran. Olahraga yang saya sukai adalah memanaha";
+    }
+}
